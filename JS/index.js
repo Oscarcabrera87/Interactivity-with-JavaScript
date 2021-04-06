6906e880323c9d434b2085d39b15f032a44efabf
OscarC <Oscarcabrera87@gmail.com>
2021-04-06 14:04:23 -0700

    Clean slate

--- JS/index.js
+++ JS/index.js
@@ -1,26 +1,4 @@
-function billingFunction(){
-  
-    let testTheBox = document.getElementById('same');
-    let shipN = document.getElementById('shippingName');
-    let shipZ = document.getElementById('shippingZip');
-    let billN = document.getElementById('billingName');
-    let billZ = document.getElementById('billingZip');
-    
-    if(testTheBox.checked){
-      billN.value = shipN.value;
-      billZ.value = shipZ.value;
-      
-      billN.setAttribute("required", true);
-      billZ.setAttribute("required", true)
-    }else{
-      billN.value = "";
-      billZ.value = "";
-      billN.removeAttribute("required");
-      billZ.removeAttribute("required");
-    }
-  
-    
-  }
+
       
   
    
